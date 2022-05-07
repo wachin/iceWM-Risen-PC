3c727cbddcab4617d8cb1cc1d05b507983edde05
wachin <wachin.id@gmail.com>
2022-05-07 11:45:48 -0500

    update

---
+++ RisenPC-Ubuntu-18.04/script-actualizar-menu-icewm.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+icewm-menu-fdo >~/.icewm/appmenu
+
+# Actualizar menu después que instales una aplicacion
+# porque si no haces esto no aparecen los nuevos iconos
+# en el menu de aplicaciones de icewm
+# Consultado en https://unix.stackexchange.com/questions/335298/convert-desktop-files-to-debian-menu-system-files
+# pero explico que modifiqué el comando para no perder mi menu
+# Att: Washington Indacochea Delgado
+# 2018-05-04
+
---
+++ RisenPC-iceWM Ubuntu 18.04 posible/.profile
@@ -0,0 +1,8 @@
+
+# CONFIGURE QT5 APPLICATION STYLE, ICONS, FONTS AND MORE WITH QT5CT
+# http://www.webupd8.org/2015/11/configure-qt5-application-style-icons.html
+
+export QT_QPA_PLATFORMTHEME="qt5ct"
+
+# Esto es necesario para que la herramienta de configuración "qt5ct" pueda
+# iniciar en iceWM
